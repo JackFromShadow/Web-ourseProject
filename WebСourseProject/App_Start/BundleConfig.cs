867abb2ac3f49ee914e66abbbd9791d0d758db76
Префект Форд <ford.prefekt@mail.ru>
2017-12-07 06:15:50 +0300

    CSS

--- WebСourseProject/App_Start/BundleConfig.cs
+++ WebСourseProject/App_Start/BundleConfig.cs
@@ -29,7 +29,8 @@ namespace WebСourseProject
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/simple-sidebar.css"));
+                      "~/Content/simple-sidebar.css",
+                      "~/Content/stylesheet.css"));
         }
     }
 }
