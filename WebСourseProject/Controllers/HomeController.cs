85558a31e4f3e060af97b52d00db9493314d8c84
Префект Форд <ford.prefekt@mail.ru>
2018-01-12 07:53:44 +0300

    Модель? Что делать с моделью? Я потерялся.
    Добавлен редактор.
    
    Добавлен перевод.
    
    Добавлен стиль, но он отстой. Надо заменить, и заменить быстро.

--- WebСourseProject/Controllers/HomeController.cs
+++ WebСourseProject/Controllers/HomeController.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Globalization;
+using System.Collections;
+using System.Web.Security;
+using WebСourseProject.Filters;
 
 namespace WebСourseProject.Controllers
 {
+    [Culture]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -25,6 +30,46 @@ namespace WebСourseProject.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult _PartialIndexArticles()
+        {
+            //var News = work.AllArticle3();
+            //return PartialView(News);
+            return PartialView();
+        }
+        public ActionResult _PartialLayoutNews()
+        {
+            // var News = db.News.ToList(); 
+            //var News = work.AllNews2();
+
+            //return PartialView(News);
+            return PartialView();
+        }
+
+        public ActionResult ChangeCulture(string lang)
+        {
+            string returnUrl = Request.UrlReferrer.AbsolutePath;
+            // Список культур
+            List<string> cultures = new List<string>() { "ru", "en"};
+            if (!cultures.Contains(lang))
+            {
+                lang = "ru";
+            }
+            // Сохраняем выбранную культуру в куки
+            HttpCookie cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;   // если куки уже установлено, то обновляем значение
+            else
+            {
+
+                cookie = new HttpCookie("lang");
+                cookie.HttpOnly = false;
+                cookie.Value = lang;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return Redirect(returnUrl);
         }
     }
 }
---
+++ WebСourseProject/Models/CathedraModels.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace WebСourseProject.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Group_Name { get; set; }
+        public int Count_Of_Student { get; set; }                 
+        public ICollection<Student> Students { get; set; }
+        public ICollection<Timetable> TimeTables { get; set; }
+    }
+
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FathersName { get; set; }
+        [Required]
+        public int GroupId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [ForeignKey("GroupId")]
+        public Group Group { get; set; }
+
+        public string imgPath { get; set; }
+    }
+
+    public class Graduate
+    {   //Выпускник
+        [Key]
+        [ForeignKey("ScienceWork")]
+        public int Id { get; set; }
+        [Required]
+        public string FIO { get; set; }
+
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+
+        public ScienceWork ScienceWork { get; set; }
+
+    }
+
+    public class ScienceWork
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Theme { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string ScienceDirection { get; set; }
+
+        public Graduate Graduate { get; set; }
+    }
+
+    public class Teacher
+    {
+        [Key]
+        public int? Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FathersName { get; set; }
+        [Required]
+        [UIHint("tinymce_jquery_full"), AllowHtml]
+        public string Information { get; set; }
+        [Required]
+        public string Qualification { get; set; }   // типа вместо Degree. Учёная степень/ранг
+        public ICollection<Group> Groups { get; set; }
+        public ICollection<Graduate> Graduates { get; set; }
+      
+        public string imgPath { get; set; }
+    }
+
+    public class Lesson
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int number { get; set; }
+        [Required]
+        public int day { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public int TimetableId { get; set; }
+
+        public string TeachersName { get; set; }
+        [ForeignKey("TimetableId")]
+        public Timetable timetable { get; set; }
+    }
+
+    public class Topic
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Header { get; set; }
+        [Required]
+        [UIHint("tinymce_jquery_full"), AllowHtml]
+        public string Content { get; set; }
+
+        public DateTime published { get; set; }
+
+        public string Type { get; set; }//Тип - новость/статья/объявление 
+
+        public string imgPath { get; set; }//путь к изображению-заголовку.Это надо.
+
+        public IEnumerable<string> Items { get; set; }
+    }
+
+
+    public class Timetable
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int GroupId { get; set; }
+ 
+
+        [ForeignKey("GroupId")]
+        public Group Group { get; set; }
+
+  //      [ForeignKey("PeriodId")]
+ //       public Period Period { get; set; }
+
+        IEnumerable<Lesson> Lessons;
+    }
+
+    public class LessonRequest
+    {
+        public string name { get; set; }
+        public string teacher { get; set; }
+    }
+
+
+    public class SiteContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+  //      public DbSet<Period> Periods { get; set; }
+        public DbSet<Timetable> Timetables { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+
+        public SiteContext()
+        : base("name=SchoolContext")
+        {
+            Database.SetInitializer<SiteContext>(new CreateDatabaseIfNotExists<SiteContext>());
+        }
+    }
+}
